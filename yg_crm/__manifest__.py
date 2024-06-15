cb5a2ce91998b5e4565fce679eca1afb96428f53
Ajaykrishna P <ajaykrishna.p@secureyes.net>
2024-06-15 14:52:20 +0530

    UPDATE

---
+++ yg_crm/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "YG CRM",
+
+    'summary': """
+            Interview Tasks
+        """,
+
+    'description': """
+        Sales Team CRM Access
+    """,
+
+    'author': "Ajaykrishnap",
+    'website': "https://www.yougotagift.com",
+    'category': 'Sales/CRM',
+    'version': '16.0.1.0.',
+    'depends': ['crm'],
+
+    # always loaded
+    'data': [
+        'security/security.xml'
+    ],
+    'application': True
+
+}
